210dc8e01cdea74eb351c88a5f1b38741ba20127
ElasticBottle <winstonyeo99@yahoo.com>
2020-08-25 15:29:20 +0800

    ♻️ Refactor image generation into its own class
    Add parser argumetns for cli interface
    
    Update README on the usage of the python file

--- README.md
+++ README.md
@@ -14,4 +14,23 @@ The summary includes:
 
 ## Using the Tool
 
-## TODO (Elastic Bottle): write it up
+### Setting up
+
+1. Ensure that you have python 3.8 installed:
+    - `pip install pipenv`
+2. After which, clone the tool.
+3. Then run  `pipenv install` in the `dir` of the tool.
+
+### Generating Phylogenetic Tree
+
+In the directory of the tool:
+
+`python phylo.py "INPUT_FILE.nwk"`
+
+Alternatively, run `python phylo.py -h` in the tool for help
+
+### Generating Clade Progression and Geo Clade Representation
+
+Run 'python graph_plot.py' in the dir of the tool for help.
+
+## Todo(elasticBottle): fill this in
--- phylo.py
+++ phylo.py
@@ -1,5 +1,6 @@
-from ete3 import Tree, TreeStyle, NodeStyle
-from typing import Union
+from ete3 import Tree, TreeNode, TreeStyle, NodeStyle
+from typing import Union, Tuple
+import argparse
 
 
 def tree_style() -> TreeStyle:
@@ -15,11 +16,11 @@ def tree_style() -> TreeStyle:
 
 def node_style(clade: str, color: str, is_leaf: bool,) -> NodeStyle:
     color_scheme = {
-        "G_": "#fa0000",
-        "GH": "#ff9933",
-        "GR": "#ff3333",
-        "S_": "LightGreen",
-        "V_": "Violet",
+        "G_": "#fdb3b3",
+        "GH": "#feab8d",
+        "GR": "#fe8d8d",
+        "S_": "#f0b3f0",
+        "V_": "#b3ffb3",
         "L_": "White",
         "O_": "White",
     }
@@ -32,39 +33,152 @@ def node_style(clade: str, color: str, is_leaf: bool,) -> NodeStyle:
     style["vt_line_type"] = 0  # 0 solid, 1 dashed, 2 dotted
     style["hz_line_type"] = 0
     style["bgcolor"] = color_scheme.get(clade, "White")
+    style["vt_line_width"] = 20
+    style["hz_line_width"] = 20
     return style
-    # style["vt_line_width"] = 2
-    # style["hz_line_width"] = 2
 
 
-def get_clade_color(name: str):
-    """
-    Args:
-        - name(str): In one of the following formats ('' is part of the string):
-            'GR_USAMNMDH1413_20200623_NorthAmerica_[&!color=ff00cc]'
-            'GR_EnglandLONDD4621_20200408_Europe_'
-    Returns:
-        - str: the clade that the taxonomy belongs too
-        - str / None : the color for that clade if any
-    """
-    name_color = name.split("color=")
-    clade = name_color[0][1:3]
-    color = None
-    if len(name_color) == 2:
-        color = name_color[1][:-2]
-    return (clade, color)
+class PhyloIMG:
+    def __init__(self, file: str):
+        self.file = file
+        self.clades_list = [
+            "G_",
+            "GH",
+            "GR",
+            "S_",
+            "V_",
+            "L_",
+            "O_",
+        ]
+        self.clades = {
+            "G_": [],
+            "GH": [],
+            "GR": [],
+            "S_": [],
+            "V_": [],
+            "L_": [],
+            "O_": [],
+        }
+        self.tree = self.get_tree(self.file)
 
+    def _update_clades(self, clade: str, node: TreeNode):
+        """
+        Update [self.clades] with the latest [clade] of the taxonomy
+        
+        Args:
+            - clade(str): The clade of the taxonomy
+            - node(TreeNode): The node to be updated
+        """
+        clade_list = self.clades.get(clade, self.clades["O_"])
+        clade_list.append(node.name)
+        if clade in self.clades_list:
+            self.clades[clade] = clade_list
+        else:
+            self.clades["O_"] = clade_list
 
-def get_tree(file: str, test: bool = False) -> Tree:
-    tree = Tree(newick=file)
-    for i, n in enumerate(tree.iter_leaves()):
-        clade, color = get_clade_color(n.name)
+    def _get_clade_color(self, name: str) -> Tuple[str, Union[str, None]]:
+        """
+        Args:
+            - name(str): In one of the following formats ('' is part of the string):
+                'GR_USAMNMDH1413_20200623_NorthAmerica_[&!color=ff00cc]'
+                'GR_EnglandLONDD4621_20200408_Europe_'
+        Returns:
+            - str: the clade that the taxonomy belongs too
+            - str / None : the color for that clade if any
+        """
+        name_color = name.split("color=")
+        clade = name_color[0][1:3]
+        color = None
+        if len(name_color) == 2:
+            color = name_color[1][:-2]
+        return (clade, color)
+
+    def _color_node(self, n: TreeNode):
+        """
+        Sets the NodeStyle for [n] and updates the clades dictionary
+        """
+        clade, color = self._get_clade_color(n.name)
         n.img_style = node_style(clade, color, n.is_leaf())
-        if test and i == 100:
-            break
-    return tree
+        self._update_clades(clade, n)
+
+    def _root_on_s(self, tree: TreeNode):
+        s_ancestor = tree.get_common_ancestor(*self.clades["S_"])
+
+        tree.set_outgroup(s_ancestor)
+
+    def _color_ancestors(self, tree: TreeNode):
+        # s_ancestor.img_style = node_style("S_", None, False)
+        for clade in self.clades_list:
+            ancestor = tree.get_common_ancestor(*self.clades.get(clade, []))
+            ancestor.img_style = node_style(clade, None, False)
+
+    def get_tree(self, file: str, test: bool = False) -> Tree:
+        """
+
+        """
+        tree = Tree(newick=file)
+
+        for i, n in enumerate(tree.iter_leaves()):
+            self._color_node(n)
+            if test and i == 1000:
+                break
+        self._root_on_s(tree)
+        # self._color_ancestors(tree)
+
+        return tree
+
+    def __call__(self, output: str, dpi: int, width: int):
+        self.tree.render(output, dpi=dpi, w=width, tree_style=tree_style())
 
 
-tree = get_tree("./data/input_sample/fnb_all_rapidnj.nwk")
-tree.render("phylo_tree.png", dpi=300, w=10000, tree_style=tree_style())
+def _parse_args():
+    """Parse command-line arguments."""
+    parser = argparse.ArgumentParser(
+        description="Generates hcov-19 Phylogentic tree based on GISAID bi-weekly analysis update",
+        epilog="If you notice any issues, please open one over at https://github.com/ElasticBottle/GISAID-Analysis-Update ",
+    )
+    parser.add_argument(
+        "file", type=str, default="", help="file path of the NEWICK tree to parse",
+    )
+    parser.add_argument(
+        "-o",
+        "--out",
+        type=str,
+        metavar="",
+        default="./out.png",
+        help="The output path of the image to be stored. Default ./out.png",
+    )
+    parser.add_argument(
+        "-d",
+        "--dpi",
+        nargs=1,
+        default=300,
+        type=int,
+        dest="dpi",
+        help="Dpi of the output image. Default 300",
+    )
+    parser.add_argument(
+        "-w",
+        "--width",
+        nargs=1,
+        default=15000,
+        dest="width",
+        help="Specifies the width of the image in pixel, the height is then automatically derived. Default 15000",
+    )
+    args = parser.parse_args()
+    return args
+
+
+def main():
+    args = _parse_args()
+    img = PhyloIMG(file=args.file,)
+    img(
+        output=args.out, dpi=args.dpi, width=args.width,
+    )
+
+
+if __name__ == "__main__":
+    main()
+# "./data/input_sample/fnb_all_rapidnj.nwk"
+# "phylo_tree_0width.png"
 
