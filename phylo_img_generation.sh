a72fc064247dee2268c65f97e5bfbb9d3e960518
Winston <winstonyeo99@yahoo.com>
2020-09-09 18:03:02 +0800

    🐛 Fix file_naming error for nwk file

--- phylo_img_generation.sh
+++ phylo_img_generation.sh
@@ -4,7 +4,7 @@ OUTPUT_FOLDER="/HOME_ann/BII/biipsashare/winston/output/"
 SCRIPTS="/HOME_ann/BII/biipsashare/winston/GISAID-Analysis-Update/"
 PYTHON="~/miniconda3/envs/phylo/bin/python"
 
-eval ${PYTHON} ${SCRIPTS}extractor.py ${BASE_PATH}raphael/treeupdate$(date --date="1 days ago" '+%Y%m%d') ${INPUT_FOLDER}
+eval ${PYTHON} ${SCRIPTS}extractor.py ${BASE_PATH}raphael/treeupdate$(date --date="1 days ago" '+%Y%m%d').tar.xz ${INPUT_FOLDER}
 
 sleep 10
 
@@ -12,6 +12,8 @@ eval ${PYTHON} ${SCRIPTS}graph_plot.py ${INPUT_FOLDER}clade_progression.tsv ${IN
 
 eval ${PYTHON} ${SCRIPTS}nwk_to_fig.py -i ${INPUT_FOLDER}fnb_all_rapidnj.nwk -o ${OUTPUT_FOLDER}
 
-eval ${PYTHON} ${SCRIPTS}phylo.py ${INPUT_FOLDER}fnb_all_rapidnj.nwk -o ${OUTPUT_FOLDER}phylo.svg >> phylo_gen.log
+eval ${PYTHON} ${SCRIPTS}phylo.py ${INPUT_FOLDER}fnb_all_rapidnj.nwk -o ${OUTPUT_FOLDER}phylo.svg >> ${OUTPUT_FOLDER}phylo_gen.log
 
-eval zip ${OUTPUT_FOLDER}tree_image_gen.zip ${OUTPUT_FOLDER}clade.png ${OUTPUT_FOLDER}geoclade.png ${OUTPUT_FOLDER}$(date '+%Y-%m_d')_Full ${OUTPUT_FOLDER}phylo.svg ${OUTPUT_FOLDER}phylo_gen.log
+eval zip ${OUTPUT_FOLDER}tree_image_gen.zip ${OUTPUT_FOLDER}clade.png ${OUTPUT_FOLDER}geoclade.png ${OUTPUT_FOLDER}$(date '+%Y-%m-%d')_Full ${OUTPUT_FOLDER}phylo.svg
+
+eval mail -a ${OUTPUT_FOLDER}tree_image_gen.zip -s "Tree auto gen test" winstonyeo99@yahoo.com < ${OUTPUT_FOLDER}phylo_gen.log
